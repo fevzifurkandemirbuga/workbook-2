3e73e64b71bac01455ff529e4a8f04e24af6cc33
fevzi <fevzifd2023@gmail.com>
2025-09-30 16:45:18 -0400

    Step-1

---
+++ src/main/java/com/pluralsight/HighScoreWins.java
@@ -0,0 +1,29 @@
+package com.pluralsight;
+
+import jdk.swing.interop.SwingInterOpUtils;
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
+public class HighScoreWins {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Please enter a game score: ");
+        String input = scan.nextLine();
+
+        String[] text = input.split("\\|");
+        String[] score = text[1].split(":");
+        int winnerIndex;
+
+        if (Integer.parseInt(score[0]) > Integer.parseInt(score[1])) {
+            winnerIndex = 0;
+        } else {
+            winnerIndex = 1;
+        }
+
+        String winner = text[0].split(":")[winnerIndex];
+        System.out.println("Winner: " + winner);
+    }
+}
